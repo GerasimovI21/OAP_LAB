c5b518d151fc37e1370472bb5ab4afcc85549050
VeronikaToporova <veronichka.toporova@list.ru>
2021-03-13 13:30:26 +0300

    laba

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                /*
+                var ERROR = 1;
+                while (ERROR == 1)
+                {
+                Console.Write("Введите ГосНомер: ");
+                string GosNOMER = Console.ReadLine();
+                char[] SIMVOLY = { 'A', 'B', 'C', 'E', 'H', 'K', 'M', 'O', 'P', 'T', 'X', 'Y' };
+                    if (GosNOMER.Length == 6)
+                    {
+                      var Proverka = true;
+                        for (int i = 0; i < 6 & Proverka; i++)
+                        {
+
+                            if (i == 1 | i == 2 | i == 3)
+                            {
+                               
+                                Proverka &= Char.IsDigit(GosNOMER[i]);
+                            }
+                            else {
+                                
+                            Proverka &= (GosNOMER[i].ToString().IndexOfAny(SIMVOLY) >= 0);
+                        }
+                            Console.WriteLine(Proverka ? "Номер соответствует ГосСтандарту...." : "Номер не соответствует ГосСтандарту....");
+                        }
+                    }
+                    else if (GosNOMER.Length == 0)
+                        ERROR++;
+                    else if (GosNOMER.Length < 6)
+                    {
+
+                        Console.WriteLine("Номер меньше ГосСтандарта");
+                    }
+                    else if (GosNOMER.Length > 6)
+                    {
+                        Console.WriteLine("Номер больше ГосСтандарта");
+                */
+            }      
+        }
+    }
+}
+
